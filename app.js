46d3e8080077556a3085cd82e2832eb7e737bd54
oppiman1992 <ttetsu472proof@gmail.com>
2022-09-19 22:31:30 +0900

    課題提出

---
+++ app.js
@@ -0,0 +1,30 @@
+let num=0;
+for (let i = 0; i < 51; i++) {
+  console.log(i);
+}
+
+if (10) {
+  console.log("今"+i + "回ループしました。");
+}
+if (20) {
+  console.log("今"+i + "回ループしました。");
+}
+if (30) {
+  console.log("今"+i + "回ループしました。");
+}
+if (40) {
+  console.log("今"+i + "回ループしました。");
+}
+if (50) {
+  console.log("今"+i + "回ループしました。");
+}
+
+if (i % 4 == 0 ){
+	console.log('4で割れる数です。'+i);
+}
+
+if(50){
+  alert(+i + "回カウントが終わりました")
+}
+
+
