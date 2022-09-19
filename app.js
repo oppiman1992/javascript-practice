dfe1b7afa35ed202fbd64ba3de095712322effdb
oppiman1992 <ttetsu472proof@gmail.com>
2022-09-20 07:31:42 +0900

    課題提出4回目

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
 let num=0;
 for (let i = 0; i < 51; i++) {
   console.log(i);
-}
 
-if (10) {
-  console.log("今"+num + "回ループしました。");
-}
-if (20) {
-  console.log("今"+ num + "回ループしました。");
-}
-if (30) {
-  console.log("今"+num  + "回ループしました。");
-}
-if (40) {
-  console.log("今"+ num + "回ループしました。");
-}
-if (50) {
-  console.log("今"+ num + "回ループしました。");
+if (i % 10 == 0) {
+  console.log('今' + i + '回ループしました。');
 }
 
 if (i % 4 == 0 ){
-	console.log('4で割れる数です。'+num );
-}
-
-if(50){
-  alert(+num + "回カウントが終わりました")
+	console.log('4で割れる数です。'+i );
 }
 
 
+if(i==50){
+  alert(+i + "回カウントが終わりました")
+}
+}
